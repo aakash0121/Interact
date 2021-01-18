923752d671edf206a8b0eed4d6fe606d271e2c3c
aakash0121 <bindalaakash60@gmail.com>
2021-01-18 09:04:24 +0530

    TP-1

--- interact/settings.py
+++ interact/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Channels
+ASGI_APPLICATION = 'interact.asgi.application'
