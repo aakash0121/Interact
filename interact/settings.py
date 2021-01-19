f2cb0b65e0b67c0a5ccb04bef42dbfa3e3a7b34b
aakash0121 <bindalaakash60@gmail.com>
2021-01-19 06:10:11 +0530

    corrected media

--- interact/settings.py
+++ interact/settings.py
@@ -134,5 +134,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
-MEDIA_URL = '/meida/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "meida_root")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
