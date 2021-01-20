bbce691d999d413b6b18bce53c96674db49ea414
aakash0121 <bindalaakash60@gmail.com>
2021-01-20 09:12:55 +0530

    updated profile feature

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileModelForm
 
 def my_profile_view(request):
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
     context = {
         'profile': profile,
+        'form': form,
     }
     return render(request, 'profiles/myprofile.html', context)
